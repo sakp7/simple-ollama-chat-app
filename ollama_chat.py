ecd09f46f082e62d39f8f8f8a96012f94e19214f
Pulijala Ram Saketh <119031981+sakp7@users.noreply.github.com>
2025-01-14 11:55:06 +0530

    Update ollama_chat.py

--- ollama_chat.py
+++ ollama_chat.py
@@ -1,24 +1,29 @@
+# Required Libraries
+
 import streamlit as st
 from langchain_ollama import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-# Output parser,ollala
 
+# Prompt Template
 prompt=ChatPromptTemplate.from_messages(
     [
 
-    ("system","You are an helpful assitant"),
+    ("system","You are an helpful AI assitant"),
     ("user","Question:{question}")
     ]
     )
+
+# Initializing Ollama
 llm=OllamaLLM(model='llama3.1:latest')
+
+# Initializing OutputParser
+output_parser=StrOutputParser()
 
 st.title("Simple LLM application with Ollama")
 a=st.text_input("enter your query")
-output_parser=StrOutputParser()
 chain=prompt|llm|output_parser
 if a:
     resp=chain.invoke({'question':a})
     st.write(resp)
-
 
